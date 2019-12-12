865c944a209867ca743925b89fd97381401a95cf
Max Taylor <max@cakerstream.com>
2019-12-12 22:26:05 +0000

    Initial commit

---
+++ Game Engine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Game Engine
+//
+//  Created by Max Taylor on 12/12/2019.
+//  Copyright © 2019 Max Taylor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Game Engine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Game Engine
+//
+//  Created by Max Taylor on 12/12/2019.
+//  Copyright © 2019 Max Taylor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
