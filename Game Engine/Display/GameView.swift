b9febf60c478780866403c98dde4974d3a6241a9
Max Taylor <twally3@users.noreply.github.com>
2019-12-15 13:31:11 +0000

    Moved some code to a deligate renderer (#7)

--- Game Engine/Display/GameView.swift
+++ Game Engine/Display/GameView.swift
@@ -2,11 +2,7 @@ import MetalKit
 
 class GameView: MTKView {
     
-    var commandQueue: MTLCommandQueue!
-    
-    var vertices: [Vertex]!
-    
-    var vertexBuffer: MTLBuffer!
+    var renderer: Renderer!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
@@ -18,36 +14,7 @@ class GameView: MTKView {
         self.clearColor = Preferences.clearColour
         self.colorPixelFormat = Preferences.mainPixelFormat
         
-        createVertices()
-        createBuffers()
-    }
-    
-    func createVertices() {
-        vertices = [
-            Vertex(position: SIMD3<Float>(0, 1, 0), colour: SIMD4<Float>(1, 0, 0, 1)),
-            Vertex(position: SIMD3<Float>(-1, -1, 0), colour: SIMD4<Float>(0, 1, 0, 1)),
-            Vertex(position: SIMD3<Float>(1, -1, 0), colour: SIMD4<Float>(0, 0, 1, 1)),
-        ]
-    }
-    
-    func createBuffers() {
-        vertexBuffer = Engine.device.makeBuffer(bytes: vertices, length: Vertex.stride * vertices.count, options: [])
-    }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
-        
-        let commandBuffer = Engine.commandQueue.makeCommandBuffer()
-        
-        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        
-        renderCommandEncoder?.setRenderPipelineState(RenderPipelineStateLibrary.state(.Basic))
-        
-        renderCommandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
-        renderCommandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
-        
-        renderCommandEncoder?.endEncoding()
-        commandBuffer?.present(drawable)
-        commandBuffer?.commit()
+        self.renderer = Renderer()
+        self.delegate = renderer
     }
 }
