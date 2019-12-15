9c7b6b222dbc0a4908efc63b476412826d31e62d
Max Taylor <twally3@users.noreply.github.com>
2019-12-15 21:15:59 +0000

    Added mouse and keyboard input (#11)

--- Game Engine/Display/Renderer.swift
+++ Game Engine/Display/Renderer.swift
@@ -1,12 +1,22 @@
 import MetalKit
 
 class Renderer: NSObject {
+    public static var screenSize = SIMD2<Float>(repeating: 0)
     
+    init(_ mtkView: MTKView) {
+        super.init()
+        updateScreenSize(view: mtkView)
+    }
 }
 
 extension Renderer: MTKViewDelegate {
+    
+    public func updateScreenSize(view: MTKView) {
+        Renderer.screenSize = SIMD2<Float>(Float(view.bounds.width), Float(view.bounds.height))
+    }
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        // Window resize
+        updateScreenSize(view: view)
     }
     
     func draw(in view: MTKView) {
