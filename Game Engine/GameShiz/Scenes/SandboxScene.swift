f53c5a17b7bbeb0a5e6c9fc0f9b96c324e01b13c
Max Taylor <max@cakerstream.com>
2020-04-20 23:54:38 +0100

    Added submesh support for custom meshes and merged custom and model mesh classes

--- Game Engine/GameShiz/Scenes/SandboxScene.swift
+++ Game Engine/GameShiz/Scenes/SandboxScene.swift
@@ -1,28 +1,25 @@
 class SandboxScene: Scene {
     let camera = DebugCamera()
-    let theSuzannes = TheSuzannes()
+    let chest = Chest()
     let sun = Sun()
     
     override func buildScene() {
-        camera.setPosition(0, 0, 10)
+        camera.setPosition(0, 0, 3)
         addCamera(camera)
         
-        sun.setPosition(SIMD3<Float>(0, 2, 2))
-        sun.setMaterialIsLit(false)
-        sun.setLightBrightness(0.3)
-        sun.setMaterialColour(1,1,1,1)
-        sun.setLightColour(1,1,1)
+        sun.setPosition(SIMD3<Float>(0, 5, 5))
         addLight(sun)
         
-        theSuzannes.setMaterialShininess(100)
         
-        addChild(theSuzannes)
+        chest.moveY(-0.5)
+        chest.setTexture(.PartyPirateParot)
+        addChild(chest)
     }
     
     override func doUpdate() {
         if (Mouse.isMouseButtonPressed(button: .LEFT)) {
-            theSuzannes.rotateX(Mouse.getDY() * GameTime.deltaTime)
-            theSuzannes.rotateY(Mouse.getDX() * GameTime.deltaTime)
+            chest.rotateX(Mouse.getDY() * GameTime.deltaTime)
+            chest.rotateY(Mouse.getDX() * GameTime.deltaTime)
         }
     }
 }
